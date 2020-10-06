d62e65d8310a274f038b611de156d8a84de546e9
Ritesh <rathodriteshrock@gmail.com>
2020-10-06 11:44:13 +0530

    h1 tag value change

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>SU key bhai</h1>,document.getElementById('root'));
+ReactDOM.render(<h1>SU key jalsa bhai tare to</h1>,document.getElementById('root'));
